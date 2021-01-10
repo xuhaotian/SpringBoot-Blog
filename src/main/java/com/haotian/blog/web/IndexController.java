2aa09c143fd70fbdaf807785fb300f31437c8bae
xuhaotian <xuhaotiancn@gmail.com>
2021-01-11 01:55:33 +1100

    page handling

--- src/main/java/com/haotian/blog/web/IndexController.java
+++ src/main/java/com/haotian/blog/web/IndexController.java
@@ -1,6 +1,5 @@
 package com.haotian.blog.web;
 
-//import javassist.NotFoundException;
 import com.haotian.blog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,18 +7,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
-    @GetMapping("/{id}/{name}")
 
-
-    public String index(@PathVariable Integer id, @PathVariable String name) {
-        //test error
+    @GetMapping("/")
+    public String index() {
+//test error
+//  public String index(@PathVariable Integer id, @PathVariable String name) {
 //        int i = 9/0;
 //        String blog =null;
 //        if(blog ==null){
 //            throw new NotFoundException("Cant find blog");
 //        }
-
-        System.out.println("------------index------------");
+//        System.out.println("------------index------------");
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+//        System.out.println("------------blog------------");
+        return "blog";
     }
 }
