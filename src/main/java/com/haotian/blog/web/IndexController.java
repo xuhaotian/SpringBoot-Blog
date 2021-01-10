2a719740b4f7024d7bbf306700a88a0722708589
xuhaotian <xuhaotiancn@gmail.com>
2021-01-11 00:30:12 +1100

    Log handle

--- src/main/java/com/haotian/blog/web/IndexController.java
+++ src/main/java/com/haotian/blog/web/IndexController.java
@@ -4,19 +4,22 @@ package com.haotian.blog.web;
 import com.haotian.blog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
-    public String index() {
+    @GetMapping("/{id}/{name}")
+
+
+    public String index(@PathVariable Integer id, @PathVariable String name) {
         //test error
 //        int i = 9/0;
+//        String blog =null;
+//        if(blog ==null){
+//            throw new NotFoundException("Cant find blog");
+//        }
 
-        String blog =null;
-        if(blog ==null){
-            throw new NotFoundException("Cant find blog");
-        }
-
+        System.out.println("------------index------------");
         return "index";
     }
 }
