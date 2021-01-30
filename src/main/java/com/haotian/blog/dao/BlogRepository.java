c013416730ce054b3bd07210b9c6b1c69a96d99a
xuhaotian <xuhaotiancn@gmail.com>
2021-01-31 05:40:00 +1100

    Finish search function

--- src/main/java/com/haotian/blog/dao/BlogRepository.java
+++ src/main/java/com/haotian/blog/dao/BlogRepository.java
@@ -1,6 +1,7 @@
 package com.haotian.blog.dao;
 
 import com.haotian.blog.po.Blog;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -12,5 +13,8 @@ public interface BlogRepository extends JpaRepository <Blog, Long>, JpaSpecifica
 
     @Query("select b from Blog b where b.recommend = true")
     List<Blog> findTop(Pageable pageable);
+
+    @Query("select b from Blog b where b.title like ?1 or b.content like ?1")
+    Page<Blog> findByQuery(String query,Pageable pageable);
 
 }
