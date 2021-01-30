c903db40b9f3036e4b203b4d723f253ffceff359
xuhaotian <xuhaotiancn@gmail.com>
2021-01-31 05:12:54 +1100

    Home page data

--- src/main/java/com/haotian/blog/service/TypeServiceImpl.java
+++ src/main/java/com/haotian/blog/service/TypeServiceImpl.java
@@ -6,7 +6,9 @@ import com.haotian.blog.po.Type;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,6 +47,15 @@ public class TypeServiceImpl implements TypeService{
     @Override
     public List<Type> listType() {
         return typeRepository.findAll();
+    }
+
+    @Override
+    public List<Type> listTypeTop(Integer size) {
+//        Sort sort = new Sort(Sort.Direction.DESC,"blogs.size");
+//        Pageable pageable = new PageRequest(0,size,sort);
+        Sort sort = Sort.by(Sort.Direction.DESC,"blogs.size");
+        Pageable pageable = PageRequest.of(0,size,sort);
+        return typeRepository.findTop(pageable);
     }
 
     @Transactional
--- src/main/java/com/haotian/blog/web/IndexController.java
+++ src/main/java/com/haotian/blog/web/IndexController.java
@@ -1,30 +1,42 @@
 package com.haotian.blog.web;
 
 import com.haotian.blog.NotFoundException;
+import com.haotian.blog.service.BlogService;
+import com.haotian.blog.service.TagService;
+import com.haotian.blog.service.TypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private BlogService blogService;
+
+    @Autowired
+    private TypeService typeService;
+
+    @Autowired
+    private TagService tagService;
 
     @GetMapping("/")
-//    public String index(@PathVariable Integer id, @PathVariable String name) {
-    public String index() {
-        //test error
-//        int i = 9/0;
-//        String blog =null;
-//        if(blog ==null){
-//            throw new NotFoundException("Cant find blog");
-//        }
-//        System.out.println("------------index------------");
+    public String index(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
+                        Model model) {
+        model.addAttribute("page",blogService.listBlog(pageable));
+        model.addAttribute("types",typeService.listTypeTop(6));
+        model.addAttribute("tag",tagService.listTagTop(10));
+        model.addAttribute("recommendBlogs",blogService.listRecommendBlogTop(8));
         return "index";
     }
 
-    @GetMapping("/blog")
+    @GetMapping("/blog/{id}")
     public String blog() {
-//        System.out.println("------------blog------------");
         return "blog";
     }
 
